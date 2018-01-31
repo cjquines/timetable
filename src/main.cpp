3c4869d8dece7a445236536d71f84c421845f630
cjquines <cjquines@yahoo.com>
2018-01-31 15:04:56 +0800

    Hard-code testing in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,26 @@
 #include <string>
 
 #include "schedule.h"
+#include "group.h"
 
 int main() {
+  Schedule config(5, 3);
+  std::cout << config.GetNumDays() << std::endl;
+  config.AddGroup(0);
+  auto grp = config.GetGroup(0);
+  grp->AddSection(0, "Locke");
+  grp->AddSection(1, "Hobbes");
+  grp->AddSection(2, "Rousseau");
+  grp->AddSubject(0, 2, 0, "DRR");
+  grp->AddSubject(1, 1, 0, "PE4");
+  grp->AddSubject(2, 2, 1, "MIL");
+  grp->AddSubject(3, 2, 1, "PPL");
+  grp->AddSubject(4, 2, 2, "BIO");
+  grp->AddSubject(5, 2, 2, "III");
+  grp->AddSubject(6, 2, 3, "PHY");
+  grp->AddSubject(7, 2, 4, "CAP");
+  config.Initialize();
+  config.InitialSchedule();
+  config.TestPrint();
   return 0;
 }
