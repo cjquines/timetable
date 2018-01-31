5392455886fda5f1336d0270a973abfac342b82e
cjquines <cjquines@yahoo.com>
2018-01-31 16:15:14 +0800

    Add NonSimultaneous::CountAll()

--- src/constraints/nonsimultaneous.cpp
+++ src/constraints/nonsimultaneous.cpp
@@ -8,7 +8,7 @@ NonSimultaneous::NonSimultaneous(Schedule* schedule)
 int NonSimultaneous::CountAssign(const int &subject, const int &section,
                                  const int &timeslot) {
   int teacher = Constraint::schedule_->GetSubject(subject)->GetTeacher();
-  return Constraint::schedule_->CountSectionsOf(teacher, timeslot) > 0;
+  return Constraint::schedule_->CountSectionsOf(teacher, timeslot);
 }
 
 int NonSimultaneous::CountSwapTimeslot(const int &section,
@@ -17,12 +17,17 @@ int NonSimultaneous::CountSwapTimeslot(const int &section,
   int lhs_teacher = Constraint::schedule_->GetTeacherOf(section, lhs_timeslot);
   int rhs_teacher = Constraint::schedule_->GetTeacherOf(section, rhs_timeslot);
   if (lhs_teacher == rhs_teacher) return 0;
-  return Constraint::schedule_->CountSectionsOf(lhs_teacher, rhs_timeslot) +
-         Constraint::schedule_->CountSectionsOf(rhs_teacher, lhs_timeslot);
+  return Constraint::schedule_->CountSectionsOf(lhs_teacher, rhs_timeslot) -
+         Constraint::schedule_->CountSectionsOf(lhs_teacher, lhs_timeslot) + 
+         Constraint::schedule_->CountSectionsOf(rhs_teacher, lhs_timeslot) -
+         Constraint::schedule_->CountSectionsOf(rhs_teacher, rhs_timeslot) + 2;
 }
 
 int NonSimultaneous::CountAll() {
   int result = 0;
-  // expose teacher iteration interface for schedule
+  for (auto it = Constraint::schedule_->GetTeachersBegin();
+       it != Constraint::schedule_->GetTeachersEnd(); it++)
+    for (int j = 0; j < Constraint::schedule_->GetNumSlots(); j++)
+      result += min(0, Constraint::schedule_->CountSectionsOf(it, j) - 1);
   return result;
 }
