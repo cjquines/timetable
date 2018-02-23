58fd74aa6adc3b4a8d9534eb02a7bf0db164c43c
cjquines <cjquines@yahoo.com>
2018-02-23 17:44:45 +0800

    Add subjecttime.h

---
+++ src/constraints/subjecttime.h
@@ -0,0 +1,23 @@
+#ifndef _TIMETABLE_CONSTRAINTS_SUBJECTTIME_H
+#define _TIMETABLE_CONSTRAINTS_SUBJECTTIME_H
+
+#include <vector>
+
+#include "constraint.h"
+
+class SubjectTime : public Constraint {
+public:
+  SubjectTime(Schedule* schedule, const int &priority, const int &subject,
+              const std::vector<int> &unassignable);
+  int CountTranslate(const int &section, const int &timeslot,
+                     const int &open_timeslot) override;
+  int CountSwapTimeslot(const int &section, const int &lhs_timeslot,
+                        const int &rhs_timeslot) override;
+  int CountAll() override;
+
+private:
+  int subject_;
+  std::vector<bool> unassignable_;
+};
+
+#endif
