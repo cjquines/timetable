a9fd7c0fad56af51fb7db0ac0422da2aa1aca91c
cjquines <cjquines@yahoo.com>
2018-11-20 22:01:36 +0800

    Change main.cpp to read from input.yaml instead

--- src/main.cpp
+++ src/main.cpp
@@ -7,106 +7,85 @@
 #include "schedule.h"
 #include "group.h"
 
+#include "yaml-cpp/yaml.h"
+
+int num_groups;
+int num_teachers;
+int num_sections;
+int num_subjects;
+std::map<std::string, int> sections_dict;
+std::map<std::string, int> teachers_dict;
+
 int main() {
-  // std::time_t start_max_5 = std::time(NULL);
+  YAML::Node input = YAML::LoadFile("input.yaml");
 
-  std::ios_base::sync_with_stdio(0);
-  // Schedule config(5, 14, std::time(nullptr));
-  // std::cout << std::time(nullptr) << std::endl;
-  Schedule config(5, 14, 1542636971);
-  config.AddTeacher(0, "Darilag");
-  config.AddTeacher(1, "Catimbang");
-  config.AddTeacher(2, "Salvador");
-  config.AddTeacher(3, "De Jesus");
-  config.AddTeacher(4, "Villareal");
-  config.AddTeacher(5, "Kath");
-  config.AddTeacher(6, "Dayrit");
-  config.AddTeacher(7, "Fernandez");
-  config.AddTeacher(8, "Edz");
-  config.AddTeacher(9, "Josh");
-  config.AddTeacher(10, "Pheobe");
-  config.AddTeacher(11, "Dogma");
-  config.AddTeacher(12, "Agustin");
-  // config.AddTeacherTime(1, 0, {6, 7, 14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 1, {6, 7, 14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 2, {6, 7, 14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 3, {6, 7, 14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 4, {6, 7, 14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1,  5, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1,  6, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1,  7, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1,  8, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1,  9, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 10, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 11, {14, 15, 22, 23, 30, 31, 38, 39});
-  // config.AddTeacherTime(1, 12, {14, 15, 22, 23, 30, 31, 38, 39});
+  Schedule config(input["days"].as<int>(), input["slots"].as<int>(),
+                  std::time(nullptr));
 
-  config.AddGroup(0);
-  auto grp = config.GetGroup(0);
-  grp->AddSection(0, "Locke");
-  grp->AddSection(1, "Rousseau");
-  grp->AddSubject(0, {3, 3, 2}, 0, "DRR");
-  grp->AddSubject(1, {2}, 0, "PE4");
-  config.AddSubjectTime(225, 1, {0, 1, 2, 3, 4, 5, 6, 7});
-  grp->AddSubject(2, {3, 3, 2}, 1, "MIL");
-  grp->AddSubject(3, {3, 3, 2}, 1, "PPL");
-  grp->AddSubject(4, {3, 3, 2}, 2, "BIO");
-  grp->AddSubject(5, {3, 3, 2}, 2, "III");
-  grp->AddSubject(6, {3, 3, 2}, 3, "PHY");
-  grp->AddSubject(7, {3, 3, 2}, 4, "CAP");
+  for (auto it : input["teachers"]) {
+    std::string name = it["name"].as<std::string>();
+    config.AddTeacher(num_teachers, name);
 
-  config.AddGroup(1);
-  grp = config.GetGroup(1);
-  grp->AddSection(2, "Hobbes");
-  grp->AddSubject(8, {3, 3, 2}, 0, "DRR");
-  grp->AddSubject(9, {2}, 0, "PE4");
-  config.AddSubjectTime(225, 9, {0, 1, 2, 3, 4, 5, 6, 7});
-  grp->AddSubject(10, {3, 3, 2}, 1, "MIL");
-  grp->AddSubject(11, {3, 3, 2}, 1, "PPL");
-  grp->AddSubject(12, {3, 3, 2}, 2, "BIO");
-  grp->AddSubject(13, {3, 3, 2}, 3, "III");
-  grp->AddSubject(14, {3, 3, 2}, 3, "PHY");
-  grp->AddSubject(15, {3, 3, 2}, 4, "CAP");
+    if (it["unassignable"])
+      config.AddTeacherTime(it["unassignable"]["priority"].as<int>(),
+                            num_teachers,
+                            it["unassignable"]["times"].as< std::vector<int> >());
 
-  config.AddGroup(2);
-  grp = config.GetGroup(2);
-  grp->AddSection(3, "11STEM1");
-  grp->AddSection(4, "11STEM2");
-  grp->AddSection(5, "11STEM3");
-  grp->AddSection(6, "11STEM4");
-  grp->AddSubject(16, {3, 3, 2}, 5, "RWS");
-  grp->AddSubject(17, {3, 3, 2}, 6, "CHM");
-  grp->AddSubject(18, {3, 3, 2}, 7, "PPP");
-  grp->AddSubject(19, {3, 3, 2}, 8, "PR1");
-  grp->AddSubject(20, {2}, 0, "PE2");
-  config.AddSubjectTime(225, 20, {0, 1, 2, 3, 4, 5, 6, 7});
-  grp->AddSubject(21, {3, 3, 2}, 9, "STA");
-  grp->AddSubject(22, {3, 3, 2}, 10, "EAP");
-  grp->AddSubject(23, {3, 3, 2}, 11, "EMT");
-  grp->AddSubject(24, {3, 3, 2}, 12, "CAL");
+    teachers_dict[name] = num_teachers++;
+  }
 
-  config.AddGroup(3);
-  grp = config.GetGroup(3);
-  grp->AddSection(7, "11STEM5");
-  grp->AddSubject(25, {3, 3, 2}, 5, "RWS");
-  grp->AddSubject(26, {3, 3, 2}, 6, "CHM");
-  grp->AddSubject(27, {3, 3, 2}, 7, "PPP");
-  grp->AddSubject(28, {3, 3, 2}, 4, "PR1");
-  grp->AddSubject(29, {2}, 0, "PE2");
-  config.AddSubjectTime(225, 29, {0, 1, 2, 3, 4, 5, 6, 7});
-  grp->AddSubject(30, {3, 3, 2}, 9, "STA");
-  grp->AddSubject(31, {3, 3, 2}, 10, "EAP");
-  grp->AddSubject(32, {3, 3, 2}, 11, "EMT");
-  grp->AddSubject(33, {3, 3, 2}, 12, "CAL");
+  for (auto it : input["groups"]) {
+    config.AddGroup(num_groups);
+    auto grp = config.GetGroup(num_groups);
 
-  config.AddSubjectGaps(0);
-  config.AddMinSubjects(0, 2);
-  config.AddEvenDismissal(25, {0, 1, 2});
-  config.AddEvenDismissal(9, {3, 4, 5, 6, 7});
-  config.AddDistinctPerDay(0);
-  config.AddNonSimultaneous(0);
-  config.AddReqFirstSubject(0);
-  config.Solve(5, 1);
+    for (auto jt : it["sections"]) {
+      std::string name = jt["name"].as<std::string>();
+      grp->AddSection(num_sections, name);
+      sections_dict[name] = num_sections++;
+    }
+
+    for (auto jt : it["subjects"]) {
+      std::string name = jt["name"].as<std::string>();
+      int teacher = teachers_dict[jt["teacher"].as<std::string>()];
+      std::vector<int> times = jt["times"].as< std::vector<int> >();
+      grp->AddSubject(num_subjects, times, teacher, name);
+
+      if (jt["unassignable"])
+        config.AddSubjectTime(jt["unassignable"]["priority"].as<int>(),
+                              num_subjects,
+                              jt["unassignable"]["times"].as< std::vector<int> >());
+
+      num_subjects++;
+    }
+
+    num_groups++;
+  }
+
+  for (auto it : input["constraints"]) {
+    std::string type = it["type"].as<std::string>();
+    int priority = it["priority"].as<int>();
+
+    if (type == "distinctPerDay") {
+      config.AddDistinctPerDay(priority);
+    } else if (type == "evenDismissal") {
+      std::vector<std::string> sections
+        = it["sections"].as< std::vector<std::string> >();
+      std::vector<int> section_ids;
+
+      for (auto jt : sections)
+        section_ids.push_back(sections_dict[jt]);
+
+      config.AddEvenDismissal(priority, section_ids);
+    } else if (type == "minSubjects") {
+      config.AddMinSubjects(priority, it["minSubjects"].as<int>());
+    } else if (type == "nonSimultaneous") {
+      config.AddNonSimultaneous(priority);
+    } else if (type == "reqFirstSubject") {
+      config.AddReqFirstSubject(priority);
+    }
+  }
+
+  config.Solve(input["seconds"].as<int>(), input["attempts"].as<int>());
   config.TestPrint();
 
   return 0;
