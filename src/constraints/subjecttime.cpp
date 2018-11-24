816749d99bbae7eff26649d44d820bd0348b4522
cjquines <cjquines@yahoo.com>
2018-11-24 15:58:03 +0800

    Style change SubjectTime

--- src/constraints/subjecttime.cpp
+++ src/constraints/subjecttime.cpp
@@ -11,14 +11,20 @@ SubjectTime::SubjectTime(Schedule *schedule, const int &priority,
   for (auto i : unassignable) unassignable_[i] = 1;
 }
 
+int SubjectTime::HalfCount(const int &section, const int &lhs_timeslot,
+                           const int &rhs_timeslot) {
+  int length = schedule_->GetLengthOf(section, lhs_timeslot);
+  int result = 0;
+  for (int i = 0; i < length; i++) {
+    result += unassignable_[rhs_timeslot + i] - unassignable_[lhs_timeslot + i];
+  }
+  return result;
+}
+
 int SubjectTime::CountTranslate(const int &section, const int &timeslot,
                                 const int &open_timeslot) {
-  if (schedule_->GetSubjectOf(section, timeslot) != subject_)
-    return 0;
-  int num_slots = schedule_->GetLengthOf(section, timeslot);
-  int result = 0;
-  for (int i = 0; i < num_slots; i++)
-    result += unassignable_[open_timeslot+i] - unassignable_[timeslot+i];
+  if (schedule_->GetSubjectOf(section, timeslot) != subject_) return 0;
+  int result = HalfCount(section, timeslot, open_timeslot);
   
   if (priority_ > 0) return result*priority_;
   return result;
@@ -26,14 +32,11 @@ int SubjectTime::CountTranslate(const int &section, const int &timeslot,
 
 int SubjectTime::CountSwapTimeslot(const int &section, const int &lhs_timeslot,
                                    const int &rhs_timeslot) {
-  int num_slots = schedule_->GetLengthOf(section, lhs_timeslot);
   int result = 0;
   if (schedule_->GetSubjectOf(section, lhs_timeslot) == subject_)
-    for (int i = 0; i < num_slots; i++)
-      result += unassignable_[rhs_timeslot+i] - unassignable_[lhs_timeslot+i];
+    result += HalfCount(section, lhs_timeslot, rhs_timeslot);
   if (schedule_->GetSubjectOf(section, rhs_timeslot) == subject_)
-    for (int i = 0; i < num_slots; i++)
-      result += unassignable_[lhs_timeslot+i] - unassignable_[rhs_timeslot+i];
+    result += HalfCount(section, rhs_timeslot, lhs_timeslot);
 
   if (priority_ > 0) return result*priority_;
   return result;
@@ -41,13 +44,12 @@ int SubjectTime::CountSwapTimeslot(const int &section, const int &lhs_timeslot,
 
 int SubjectTime::CountAll() {
   int result = 0;
-  for (auto it = schedule_->GetSectionsBegin();
-       it != schedule_->GetSectionsEnd(); it++)
-    for (int i = 0; i < schedule_->GetNumSlots(); i++)
-      if (unassignable_[i] &&
-          schedule_->GetSubjectOf((*it)->GetId(),
-            schedule_->GetHeadOf((*it)->GetId(), i)) == subject_)
-        result++;
+  for (const auto it& : schedule_->GetSections()) {
+    for (int i = 0; i < schedule_->GetNumSlots(); i++) {
+      if (unassignable_[i] && schedule_->GetSubjectOf(it->GetId(),
+          schedule_->GetHeadOf(it->GetId(), i)) == subject_) result++;
+    }
+  }
 
   if (priority_ > 0) return result*priority_;
   return result;
