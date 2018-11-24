4030edc7a49bcd64ab141d2b9d0a9fb91b5779f8
cjquines <cjquines@yahoo.com>
2018-11-24 12:42:50 +0800

    Style change ReqFirstSubject

--- src/constraints/reqfirstsubject.cpp
+++ src/constraints/reqfirstsubject.cpp
@@ -19,12 +19,12 @@ int ReqFirstSubject::CountTranslate(const int &section, const int &timeslot,
 int ReqFirstSubject::CountAll() {
   int result = 0;
   
-  for (auto it = schedule_->GetSectionsBegin();
-       it != schedule_->GetSectionsEnd(); it++)
-    for (int i = 0; i < schedule_->GetNumDays(); i++)
-      if (schedule_->GetSubjectOf((*it)->GetId(),
-          i*schedule_->GetNumSlotsPerDay()) == -1)
+  for (const auto &it : schedule_->GetSections()) {
+    for (int i = 0; i < schedule_->GetNumDays(); i++) {
+      if (schedule_->IsFree(it->GetId(), i*schedule_->GetNumSlotsPerDay()))
         result++;
+    }
+  }
 
   if (priority_ > 0) return result*priority_;
   return result;
